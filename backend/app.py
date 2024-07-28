a686c908632c800dca680b91a1900ff7fc77f648
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-28 17:29:36 +0530

    Auth routes implmemented and Tested with password hashing and jwt

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,22 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify , g
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 import pika
 from dotenv import load_dotenv
 from db import create_user, find_user_by_email, create_flight, get_all_flights, get_flight_by_number, log_notification
-
+from auth import encode_auth_token, decode_auth_token
+from functools import wraps
 
 #load the environment variables
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+def convert_to_serializable(flight):
+    flight['_id'] = str(flight['_id'])
+    return flight
 
 @app.route('/')
 def home():
@@ -26,14 +31,40 @@ def signup():
         return jsonify({'message': 'Invalid data'}), 400
 
     #print the data
-    print(data)
+    #print(data)
 
     user = find_user_by_email(data['email'])
-    print(user)
+    #print(user)
     if user:
         return jsonify({'message': 'User already exists'}), 409
+    
+    hashed_password = generate_password_hash(data['password'], method='pbkdf2:sha256')
+    user_data = {
+        'name': data['name'],
+        'email': data['email'],
+        'password': hashed_password
+    }
+    #return jsonify({"user_data" : user_data}), 201
+    create_user(user_data)
+    user = find_user_by_email(data['email'])
+    token = encode_auth_token(str(user['_id']))
+    return jsonify({"jwt" : token}),201
 
-    return jsonify({"message":"Bhai aa rha hai data aur print bhi ho rha hai"})
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    data = request.json
+    if not data or not 'email' in data or not 'password' in data:
+        return jsonify({'message': 'Invalid data'}), 400
+    
+    user = find_user_by_email(data['email'])
+    if not user or not check_password_hash(user['password'], data['password']):
+        return jsonify({'message': 'Invalid email or password'}), 401
+    
+    token = encode_auth_token(str(user['_id']))
+    return jsonify({'token': token}), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
