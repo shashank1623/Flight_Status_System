40305de10d361426700c6035b74d4a29e7b65470
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-07-28 10:37:41 +0530

    Populated The Db with Flight Details

--- backend/test.py
+++ backend/test.py
@@ -12,6 +12,7 @@ client = MongoClient(MONGODB_URI)
 db = client['Indigo_flight_status_db']
 
 # Sample Data
+"""
 users = [
     {
         "name": "Shashank Bhardwaj",
@@ -19,11 +20,65 @@ users = [
         "password": "Hello@123"  # Replace with actual hashed password
     }
 ]
-
+"""
 flights = [
     {
-        "flightNumber": "AA101",
-        "airline": "American Airlines",
+        "flightNumber": "SA201",
+        "airline": "SingaPore Airlines",
+        "source": "HKG",
+        "destination": "JFK",
+        "departureTime": "2024-07-27T10:00:00Z",
+        "arrivalTime": "2024-07-27T13:00:00Z",
+        "status": "On Time"
+    },
+    {
+        "flightNumber": "UA201",
+        "airline": "United Airlines",
+        "source": "JFK",
+        "destination": "LAX",
+        "departureTime": "2024-07-27T10:00:00Z",
+        "arrivalTime": "2024-07-27T13:00:00Z",
+        "status": "On Time"
+    },
+    {
+        "flightNumber": "DL201",
+        "airline": "Delta Airlines",
+        "source": "JFK",
+        "destination": "LAX",
+        "departureTime": "2024-07-27T10:00:00Z",
+        "arrivalTime": "2024-07-27T13:00:00Z",
+        "status": "On Time"
+    },
+    {
+        "flightNumber": "BA201",
+        "airline": "British Airways",
+        "source": "JFK",
+        "destination": "LAX",
+        "departureTime": "2024-07-27T10:00:00Z",
+        "arrivalTime": "2024-07-27T13:00:00Z",
+        "status": "On Time"
+    },
+    {
+        "flightNumber": "AF201",
+        "airline": "Air France",
+        "source": "JFK",
+        "destination": "LAX",
+        "departureTime": "2024-07-27T10:00:00Z",
+        "arrivalTime": "2024-07-27T13:00:00Z",
+        "status": "On Time"
+    },
+    {
+        "flightNumber": "EK201",
+        "airline": "Emirates",
+        "source": "JFK",
+        "destination": "LAX",
+        "departureTime": "2024-07-27T10:00:00Z",
+        "arrivalTime": "2024-07-27T13:00:00Z",
+        "status": "On Time"
+    },
+    {
+        "flightNumber": "TK201",
+        "airline": "Turkish Airlines",
         "source": "JFK",
         "destination": "LAX",
         "departureTime": "2024-07-27T10:00:00Z",
@@ -31,7 +86,7 @@ flights = [
         "status": "On Time"
     }
 ]
-
+"""
 notifications = [
     {
         "flightNumber": "AA101",
@@ -39,11 +94,11 @@ notifications = [
         "timestamp": "2024-07-27T08:00:00Z"
     }
 ]
-
+"""
 async def populate_db():
-    db['users'].insert_many(users)
+    #db['users'].insert_many(users)
     db['flights'].insert_many(flights)
-    db['notifications'].insert_many(notifications)
+    #db['notifications'].insert_many(notifications)
     print("Database populated with sample data.")
 
 if __name__ == "__main__":
